f4b147088de91415e1bf4328adcef5cd0c393326
Jakub Habdas <104032948+Pi3gus@users.noreply.github.com>
2023-05-29 19:34:24 +0200

    7 task

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    let reverseString = '';
+    for(let i = text.length - 1; i >= 0; i--){
+        reverseString += text[i];
+    }
+    return reverseString;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    let output = 0;
+    if (!Number.isInteger(start) || !Number.isInteger(end)) return 'ERROR';
+    if(Math.min(start, end) > 0){
+        for(let i = Math.min(start, end); i <= Math.max(start,end); i++){
+            output += i;
+        }
+    }else{
+        output = 'ERROR';
+    }
+    return output;
 };
-
 // Do not edit below this line
 module.exports = sumAll;
